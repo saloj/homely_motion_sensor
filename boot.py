b0bd3b2c1fdd2d05ef6dd6e73782c966b1b15305
Salo <js225fg@student.lnu.se>
2021-08-01 15:23:48 +0200

    final commit for now

--- boot.py
+++ boot.py
@@ -6,11 +6,10 @@ wlan = WLAN(mode=WLAN.STA)
 
 nets = wlan.scan()
 for net in nets:
-    if net.ssid == keys.wifi_ssid:
+    if net.ssid == keys.wifi_ssid: # if a WiFi network matching the ssid specified in keys.py is found
         print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, keys.wifi_password), timeout=5000)
+        wlan.connect(net.ssid, auth=(net.sec, keys.wifi_password), timeout=5000) # attemps to authorize against the network using the password in keys.py
         while not wlan.isconnected():
             machine.idle() # save power while waiting
         print('WLAN connection succeeded!')
-        print(wlan.ifconfig())
         break
--- main.py
+++ main.py
@@ -1,35 +1,38 @@
+# import the necessary libraries which contain functionality and functions
 import pycom
 import time
 from machine import Pin
 from machine import Timer
 
+# turn off the blinking LED on the LoPy4
 pycom.heartbeat(False)
 
-# config
+# basic config setup to make the alert function cleaner and easier to read
 motionDetected = 1
 noMotionDetected = 0
 hold_time_sec = 0.2
-pir = Pin('P23', mode = Pin.IN)
+pir = Pin('P23', mode = Pin.IN) # if you connected the signal to any other pin, replace 'P23' with the one you're using
 green = 0x00FF00  # green
 red = 0xFF0000  # red
-chrono = Timer.Chrono() # measure the amount of time that has passed
+chrono = Timer.Chrono() # measure the amount of time that has passed since the program started to run
 
 def alert():
-    print(chrono.read(), "Motion Detected!")
-    print(pir())
-    pycom.rgbled(red)
+    print(chrono.read(), "Motion Detected!") # prints time elapsed and a string to the console
+    pybytes.send_signal(1, 1) # sends a signal to Pybytes. the first argument is the signal number specified on Pybytes, the second is just an integer representation of an event
+    pycom.rgbled(red) # turns the LED red when detection has occurred
     time.sleep(5) # sleep for 5 seconds after detection
-    pycom.rgbled(green)
+    pycom.rgbled(green) # turns the LED green
 
 # start detection
 chrono.start()
 pycom.rgbled(green)
 print("Starting Detection...")
 
+# infinite loop, runs until terminated
 while True:
-    if pir() == motionDetected:
+    if pir() == motionDetected: # if the PIR sensor has been triggered, call the alert function
         alert()
     else:
         pass
 
-    time.sleep(hold_time_sec)
+    time.sleep(hold_time_sec) # wait for the specified amount of time to avoid cluttering and faulty readings
